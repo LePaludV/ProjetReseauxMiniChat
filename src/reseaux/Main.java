b6679638ad2edb4b93f4d707045c1510649095c3
LePaludV <59608384+LePaludV@users.noreply.github.com>
2021-11-16 15:20:40 +0100

    Fin 1er tp

--- src/reseaux/Main.java
+++ src/reseaux/Main.java
@@ -1,32 +1,92 @@
 package reseaux;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.Reader;
 import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.UnknownHostException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;	
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		String host="localhost";
+		String host="192.168.43.4";
 		InetAddress address;
+		ServerSocket serverSocket = null;
+		Socket socketOfServer = null;
+		
+
 		try {
-			address=InetAddress.getByName(host);
-			System.out.println(host+ " "+ address.getHostAddress());
+			//address=InetAddress.getByName(host);
+			System.out.println(InetAddress.getLocalHost().getHostAddress());
+			//System.out.println(host+ " "+ address.getHostAddress());
 		}
 		catch(UnknownHostException e) {
 			e.printStackTrace();
 		}
-		// TODO Auto-generated method stub
+		
 		try {
-			ServerSocket serverSocket = new ServerSocket(4444);
+			serverSocket = new ServerSocket(4444);
 			
 		}
 		catch(IOException e) {
 			System.out.println("Couldn't listen on port 4444");
-			System.out.println(-1);
+			System.exit(-1);
 		}
 		
+		Socket clientSocket =null;
+		try {
+	           System.out.println("Server is waiting to accept user...");
 
+	           // Accept client connection request
+	           // Get new Socket at Server.    
+	           clientSocket = serverSocket.accept();
+	           System.out.println("Accept a client!");
+
+	          
+	           Socket echoSocket = null;
+	           PrintWriter out= null;
+	           BufferedReader in=null;
+	           
+	           
+	           try {
+	        	   echoSocket= new Socket("127.0.1.1",4444);
+	        	   out = new PrintWriter(echoSocket.getOutputStream(),true);
+	        	   in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+	        	   
+	        	   
+	           }catch(UnknownHostException e) {
+	        	   System.err.println("Don't know about host taranis.");
+	        	   System.exit(-1);
+	           }catch(IOException e) {
+	        	   System.err.println("Couldn't get I/O for "+"the connection to taranis");
+	        	   System.exit(-1);
+	           }
+
+	          BufferedReader strIn=new BufferedReader(new InputStreamReader(System.in));
+
+	          String userInput;
+	          while((userInput=strIn.readLine()) != null) {
+	        	  System.out.println(userInput);
+	          
+	          }
+	          
+	          
+	       } catch (IOException e) {
+	           System.out.println(e);
+	           e.printStackTrace();
+	       }
+	       System.out.println("Sever stopped!");
+	   }
 	}
-
-}
